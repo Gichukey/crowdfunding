4a194b99d66b16bccb0d3a13a229c7548812483f
Gichukey <denniskori@gmail.com>
2019-01-28 14:36:30 +0300

    Added app.js
    Co-Authored-By: EddyKasila <eddykasila@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,28 @@
+// Configuration
+var CAMPAIGN_GOAL = 1000; // Your fundraising goal, in dollars
+
+// Initialize an Express app
+var express = require('express');
+var app = express();
+app.use("/static", express.static(__dirname + '/static')); // Serve static files
+app.use(express.bodyParser()); // Can parse POST requests
+app.listen(1337); // The best port
+console.log("App running on http://localhost:1337");
+
+// Serve homepage
+app.get("/",function(request,response){
+
+    // TODO: Actually get fundraising total
+    response.send(
+        "<link rel='stylesheet' type='text/css' href='/static/fancy.css'>"+
+        "<h1>Your Crowdfunding Campaign</h1>"+
+        "<h2>raised ??? out of $"+CAMPAIGN_GOAL.toFixed(2)+"</h2>"+
+        "<a href='/fund'>Fund This</a>"
+    );
+
+});
+
+// Serve funding page
+app.get("/fund",function(request,response){
+    response.sendfile("fund.html");
+});
